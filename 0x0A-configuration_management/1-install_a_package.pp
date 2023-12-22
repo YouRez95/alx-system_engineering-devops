80ae5ba49719d514d4676e57176a59fd6ac72a0e
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-22 22:17:50 +0100

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,8 @@
 # install flask with pip3
-class { 'python':
-   version => '3',
-}
-
-python::pip { 'Flask':
-   ensure    => '2.1.0',
-   provider  => 'pip3',
+class flask {
+   package { 'flask':
+      version => '3',
+      ensure    => '2.1.0',
+      provider  => 'pip3',
+   }
 }
