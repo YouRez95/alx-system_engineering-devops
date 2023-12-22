544f8d418721f91b327609e38491079c33fff137
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-22 21:41:32 +0100

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create a file in /tmp
 
 file { 'school':
-	ensure  => 'present',
-	content => 'I love Puppet',
-	owner   => 'www-data',
-	mode    => '0744',
-	group   => 'www-data',
-	path    => '/tmp/school',
+       ensure  => 'present',
+       content => 'I love Puppet',
+       owner   => 'www-data',
+       mode    => '0744',
+       group   => 'www-data',
+       path    => '/tmp/school',
 }
