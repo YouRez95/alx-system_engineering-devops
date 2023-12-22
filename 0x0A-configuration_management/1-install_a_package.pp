d237b804e5734651f71fee837e95a4ec710a2f96
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-22 22:28:05 +0100

    kill process

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,8 @@
 # install flask with pip3
 class flask {
-   package { 'flask':
-      version => '3',
-      ensure    => '2.1.0',
-      provider  => 'pip3',
-   }
+  package { 'flask':
+    version => '3',
+    ensure    => '2.1.0',
+    provider  => 'pip3',
+  }
 }
