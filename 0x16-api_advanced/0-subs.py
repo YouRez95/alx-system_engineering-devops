998d90cf240a7e56408b4fd561636bfc8c8c0336
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-13 12:22:18 +0100

    change acces by key to get

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,10 +9,11 @@ def number_of_subscribers(subreddit):
     """
       function that use the request module to send get request
     """
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit))
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'user-agent': "test"})
 
     if r.status_code != 200:
         return 0
     response = r.json()
-    num_subscribers = response['data']['subscribers']
+    num_subscribers = response.get('data').get('subscribers')
     return num_subscribers
