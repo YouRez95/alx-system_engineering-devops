f7847771bbddaddf369a895222a9f98f46089599
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-13 20:39:26 +0100

    get the 10 top hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module that get the 10 hot posts for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+      send the get request
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(
+        url, headers={'User-Agent': 'test'}, allow_redirects=False)
+
+    if response.status_code in range(300, 500):
+        print(None)
+        return
+
+    data = response.json().get('data', {}).get('children', [])
+    for i in range(10):
+        print(data[i].get('data', {}).get('title'))
