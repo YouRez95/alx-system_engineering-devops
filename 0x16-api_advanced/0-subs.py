ee9640a2773cdd7ea25c28d4a1b998f57a45a993
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-13 12:37:23 +0100

    chech the params

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,8 @@ def number_of_subscribers(subreddit):
     """
       function that use the request module to send get request
     """
+    if not subreddit or type(subreddit) is not str:
+        return 0
     r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'user-agent': "test"})
     response = r.json()
