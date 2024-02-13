b2fa67b06897eb1aebaa7c6aca91620b19d70234
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-13 12:03:20 +0100

    python file request the number of subscribers from reddit api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module that get the number of subscibers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'user-agent': "test"})
+
+    if r.status_code == 404:
+        return 0
+    response = r.json()
+    num_subscribers = response['data']['subscribers']
+    return num_subscribers
