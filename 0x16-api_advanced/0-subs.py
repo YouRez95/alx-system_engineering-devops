a60a580a37885d6114b407c5af338354895d07df
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-13 19:53:33 +0100

    add range

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,8 @@ def number_of_subscribers(subreddit):
       function that use the request module to send get request
     """
 
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=False)
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'test'})
     if r.status_code in range(300, 404):
         return 0
     response = r.json()
