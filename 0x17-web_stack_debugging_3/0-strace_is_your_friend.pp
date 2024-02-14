26f90cadceec966fa6f35776cde2847804f580bc
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-14 20:24:08 +0100

    replace phpp with php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,4 @@
-file { '/var/www/html/wp-settings.php':
-  ensure => file,
-}
-
-exec { 'replace_line':
+exec { 'replace_phpp':
   command => "grep -q 'phpp' /var/www/html/wp-settings.php && sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/bin:/usr/bin',
-  require => File['/var/www/html/wp-settings.php'],
 }
