f7e6ba4af19538fb27d11ea377e3300f12e56d41
Youness Elalouani <younesselalouani1995@gmail.com>
2024-02-14 20:27:52 +0100

    add the path of the prob-vider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix the phpp extension in wp-setting file
 exec { 'replace_phpp':
   command  => "grep -q 'phpp' /var/www/html/wp-settings.php && sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  provider => 'shell',
+  provider => '/bin:/usr/bin',
 }
